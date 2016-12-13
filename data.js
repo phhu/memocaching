602afc646162a0d9a7e1f1e0a2f1ffb03aaca3cf
phhu <phhu.git@gmail.com>
2016-12-13 11:10:24 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,118 @@
+var locData = [
+	{
+		'name':'openeat!'
+		,'locations': [
+			'pirate.forms.belly'
+			,'forget.vest.hammer'
+			,'clip.fuzzy.charm'
+			,'posts.author.fall'
+			,'young.rests.bath'
+		]
+		,'lat':55.866901
+		,'long':-4.261366
+		,'tolerance':500
+		,'media': [
+			{'type':'video', 'url': 'https://www.youtube.com/embed/3hFgdHhB1mQ'}     //mXbdP5x_75c
+		]
+		,'tags': 
+			{'district':'garnethill'}
+		,'text':'<p><b>NEXT OPEN EAT EVENT AFTER THE 21ST OF AUGUST!!! JOIN US!</b></p><p>A casual visitor to Scotland might observe the variety of watering holes and 			conclude that drink is an important lubricant of “cultural integration.” But what 			of food? Is there life beyond the ubiquitous deep fat fryer? Do we make the most 			of this most universal of human appetites, creating occasions for sociability 			and exchange as well as for sustenance? Do we make good use of the variety of 			culinary knowledge that the city possesses, using it to flavour our streets as well 			as lining our stomachs? To broaden our horizons as well as our waistlines? 			In many countries the sights, sounds and smells of food being prepared, sold 			and consumed are an inescapable part of everyday street life. In Scotland 			these rituals often take place out of the public eye: testament doubtless to the 			vagaries of our maritime climate, but also perhaps to an untapped opportunity 			to encourage social interactions, to reinforce the sense of place and belonging 			essential to a well-functioning city, and to foster the exchange of ideas and 			narratives necessary for creativity and prosperity to flourish.</p><p>OpenEat is a movement for the sharing of food and dialogue in public. 			It observes that eating is a need and a pleasure common to us all, 			and that when we sit and share food together we also open ourselves 			to others. By offering principles and tools for making this easier to do, 			OpenEat hopes to make our society more inclusive, creative, happy, 			prosperous — and better fed.</p>'
+	}
+	,{
+		'name':'gibson'
+		,'locations': [
+			'woven.asset.trade'
+			,'polite.ever.relax'
+			,'holly.rating.voting'
+			,'soap.harsh.pounds'
+			,'sing.cans.seats'
+			,'emerge.rift.stops'
+			,'rental.lost.year'
+			,'hips.known.cooks'
+			,'pulled.pill.asking'
+			,'dome.times.added'
+			,'locals.candy.dates'
+			,'begin.same.notes'
+		]
+		,'lat': 55.872237
+		,'long':-4.282271
+		,'tolerance': 500
+		,'media': [
+			{'type':'video', 'url': 'https://www.youtube.com/embed/HH7B-NWRoNM'}
+		]
+		,'tags': 
+			{'district':'hillhead'}
+	,'text':'<p><b>NEXT PIZZA KUCHA AFTER THE FINAL PRESENTATION!!! JOIN US!</b></p><p>A casual to Gibson Street might observe all the take aways and students and assume it is a street like any other. But on that Street lives one of Italy\' finest migrant pizza chefs...</p>'	
+	}
+	,{
+		'name':'mackintosh'
+		,'locations': [
+			'local.flat.plates'
+		]
+		,'lat': 55.866147
+		,'long':-4.264009
+		,'tolerance': 200
+		,'media': [
+			{'type':'video', 'url': 'https://www.youtube.com/embed/F-BnPeEhmpk'}
+		]
+		,'tags': 
+			{'district':'garnethill'}
+	,'text':'<p><b>The Glasgow School of Art\'s Machintosh building suffered a major fire on 23rd May 2014.</b></p>'	
+	}				
+	/*,{
+		'name':'haldane'
+		,'locations': [
+			'evenly.form.king'
+			,'flight.sadly.units'
+			,'fantastic.often.retail'
+			,'taken.digs.call'
+			,'rushed.lined.units'
+		]
+		,'media': [
+			{'type':'video', 'url': 'http://www.gsa.ac.uk'}
+		]
+		,'tags': 
+			{'district':'garnethill'}
+		
+	}
+	,{
+		'name':'glasgowst'
+		,'locations': [
+			'odds.loud.recall'
+			,'edge.pilots.shaped'
+			,'rips.belts.lime'
+			,'amount.gasp.soap'
+			,'words.rotate.legal'
+			,'fever.bleak.fires'
+			,'clouds.trace.salsa'
+			,'posed.assure.cans'
+			,'clocks.hosts.straw'
+
+		]
+		,'media': [
+			{'type':'video', 'url': 'https://www.youtube.com/embed/Je1AovIWzq8'}
+		]
+		,'tags': 
+			{'district':'hillhead'}
+		
+	}*/
+	,{
+		'name':'bothwell'
+		,'locations': [
+			'dunes.fresh.holds'
+			,'overnight.logo.slowly'
+			,'soon.scouts.ideas'
+			,'less.bolt.thank'
+			,'cliff.demand.affair'
+		]
+		,'lat':55.8610773
+		,'long': -4.260296
+		,'tolerance':400
+		,'media': [
+			{'type':'video', 'url': 'https://www.youtube.com/embed/ZgvknhPHnmQ'}    
+		]
+		,'tags': 
+			{'district':'city centre'}
+		,'text':'<p>An ideal location near Central Station.</p>'					
+	}
+];
